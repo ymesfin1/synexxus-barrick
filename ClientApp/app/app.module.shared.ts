1f4dca64a267287531bb3534850724aa8db849af
Yebio Mesfin <yebiomesfin@Yebios-MacBook-Pro.local>
2017-07-07 13:37:46 -0400

    Barrick Web Application intiatial setup.

---
+++ ClientApp/app/app.module.shared.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './components/app/app.component'
+import { NavMenuComponent } from './components/navmenu/navmenu.component';
+import { NavHeaderComponent } from './components/headernav/navheader.component';
+import { HomeComponent } from './components/home/home.component';
+import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
+import { CounterComponent } from './components/counter/counter.component';
+
+export const sharedConfig: NgModule = {
+    bootstrap: [ AppComponent ],
+    declarations: [
+        AppComponent,
+        NavMenuComponent,
+        NavHeaderComponent,
+        CounterComponent,
+        FetchDataComponent,
+        HomeComponent
+    ],
+    imports: [
+        RouterModule.forRoot([
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: 'home', component: HomeComponent },
+            { path: '**', redirectTo: 'home' }
+        ])
+    ]
+};
---
+++ ClientApp/app/components/navheader/navheader.component.ts
@@ -0,0 +1,10 @@
+mport { Component } from '@angular/core';
+
+@Component({
+    selector: 'nav-header',
+    templateUrl: './navheader.component.html',
+    styleUrls: ['./navheader.component.css']
+})
+export class NavHeaderComponent {
+}
+
